7603d3cdb1a7e818327ce308c7303f55ff3de5fb
Gael-Saiegh <gaesaiegh@gmail.com>
2026-06-09 12:14:05 -0300

    falta una banda

--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,27 @@ DARIO       |       50%
 
 // COMPLETAR: Implementar la función calcularTotal que reciba el id del artista, la cantidad de entradas y un código de descuento (opcional) y devuelva el precio total a pagar en patacones.
 
+let primerprecio 
+
 cuandoPasa('seleccionarArtista', ({ id, cantidad, codigoDescuento }) => {
   let cantidadNum: number = Number(cantidad);
   let precio: number = calcularTotal(id, cantidadNum, codigoDescuento);
   enviarAlFrontend('mostrarPrecio', precio);
 });
 
+let id: string = 
 iniciar();
+
+function calcularTotal( id: string, cant: number, codigo: string): number {
+
+if (id=== "sabrina") { 
+ primerprecio = 1000 
+
+}
+else if (id==="kgatlw")
+primerprecio = 700
+}
+ 
+
+
+ 
